9106ec2dd84dab5edc6e7564cab9ca3d85930f4e
Sergey Lisoviy <serlipaua@gmail.com>
2022-10-19 23:50:39 +0300

    final fixes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import 'modern-normalize/modern-normalize.css';
 import { App } from 'App/App';
 import './index.css';
 
-const a = {
-  a1: 11,
-  a2: 'hellow',
-};
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
